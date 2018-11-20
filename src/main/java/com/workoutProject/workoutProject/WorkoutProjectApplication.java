1a49a7989617081ef88f03cbdccfab7a096f4e4b
michalk933 <michalkuchciak93@gmail.com>
2018-11-20 19:59:06 +0100

    Initial commit

---
+++ src/main/java/com/workoutProject/workoutProject/WorkoutProjectApplication.java
@@ -0,0 +1,12 @@
+package com.workoutProject.workoutProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutProjectApplication.class, args);
+	}
+}
