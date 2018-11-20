5ed3e360f809da8be79f9458045ae66403114678
michalk933 <michalkuchciak93@gmail.com>
2018-11-20 20:31:59 +0100

    dev branch init

--- src/main/java/com/workoutProject/workoutProject/WorkoutProjectApplication.java
+++ src/main/java/com/workoutProject/workoutProject/WorkoutProjectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WorkoutProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WorkoutProjectApplication.class, args);
 	}
